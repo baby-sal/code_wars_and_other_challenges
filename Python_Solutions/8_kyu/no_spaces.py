6e17b16f86568f39c4951d7573c1c63562536d74
baby-sal <sally.facebook123@gmail.com>
2024-10-16 05:50:17 +0100

    completed no_spaces and what_is_between 8kyu

---
+++ Python_Solutions/8_kyu/no_spaces.py
@@ -0,0 +1,6 @@
+# Write a function that removes the spaces from the string, then return the resultant string.
+
+def no_space(x):
+    return x.replace(" ", "")
+
+print (no_space("8 j 8   mBliB8g  imjB8B8  jl  B"))
