31f5482c22a4799062f49be13d7f0f8f379e4a68
baby-sal <sally.facebook123@gmail.com>
2025-04-16 20:37:23 +0100

    tournament tournament_winner

---
+++ Python_Solutions/Algo_Expert/tournament_winner.py
@@ -0,0 +1,9 @@
+# def tournamentWinner(competitions, results):
+#     league_dict = {}
+#     for comp in competitions:
+#         for team in comp:
+#             if results[i] == 1:
+
+competitions = [["HTML", "C#"], ["C#", "Python"], ["Python", "HTML"]]
+
+print(list(enumerate(competitions)))
